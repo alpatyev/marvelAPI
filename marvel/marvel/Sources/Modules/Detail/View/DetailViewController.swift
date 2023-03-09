1a3e5fc12625f25b34d82b24d2629035473b4389
AlpatyevNikita <chococacaococonut@gmail.com>
2023-03-09 19:22:49 +0300

    deleted storyboard, default vc and changed project structure

---
+++ marvel/marvel/Sources/Modules/Detail/View/DetailViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+// MARK: - Detail view controller
+
+final class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ marvel/marvel/Sources/Modules/List/View/ListViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+// MARK: - List view controller
+
+final class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
