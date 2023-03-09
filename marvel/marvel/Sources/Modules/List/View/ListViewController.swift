8e85d0b22cac1526c5a638e21133152957ef846b
AlpatyevNikita <chococacaococonut@gmail.com>
2023-03-09 21:12:05 +0300

    added packages

--- marvel/marvel/Sources/Modules/List/View/ListViewController.swift
+++ marvel/marvel/Sources/Modules/List/View/ListViewController.swift
@@ -1,11 +1,41 @@
 import UIKit
+import SnapKit
+import Alamofire
+import RxSwift
+import RxCocoa
 
 // MARK: - List view controller
 
 final class ListViewController: UIViewController {
+    
+    // MARK: - View model
+    
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        setupRx()
+        setupView()
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    // MARK: - Setups
+    
+    private func setupRx() {
+        
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .darkGray
+        print("assert")
+    }
+    
+    private func setupHierarchy() {
+        
+    }
+    
+    private func setupLayout() {
+        
     }
 }
